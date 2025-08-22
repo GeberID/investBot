53d70b09118c8ab74a1a5cc8645cbc8b7905076d
alexander <gusljannikov@protei.ru>
2025-08-22 17:22:04 +0300

    fix

--- src/main/java/org/invest/bot/invest/core/modules/balanse/BalanceService.java
+++ src/main/java/org/invest/bot/invest/core/modules/balanse/BalanceService.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.invest.bot.core.DataConvertUtility.getPercentCount;
 import static org.invest.bot.invest.core.modules.balanse.BalanceModuleConf.*;
@@ -43,11 +44,12 @@ public class BalanceService {
                 case TARGET_PROTECTION_PERCENTAGE:
                     currentValue = coreReserveProtectionValues.get("protectionValue");
                     break;
-                case SATELLITE_CONCENTRATION_LIMIT, ALLOCATION_TOLERANCE: continue;
+                case SATELLITE_CONCENTRATION_LIMIT, ALLOCATION_TOLERANCE:
+                    continue;
             }
             checkAndAddDeviation(classDeviations, target, currentValue, totalValue);
         }
-        return new AnalysisResult(classDeviations, concentrationProblems(totalValue,instrumentObjs));
+        return new AnalysisResult(classDeviations, concentrationProblems(totalValue, instrumentObjs));
     }
 
     public Map<BalanceModuleConf, BigDecimal> calculateActualDistribution(Portfolio portfolio, List<InstrumentObj> instrumentObjs) {
@@ -61,57 +63,51 @@ public class BalanceService {
         distribution.put(TARGET_STOCK_SATELLITE__PERCENTAGE, getPercentCount(totalValue, coreReserveProtectionValues.get("satelliteStockValue")));
         distribution.put(TARGET_BOND_PERCENTAGE, getPercentCount(totalValue, portfolio.getTotalAmountBonds().getValue()));
         distribution.put(TARGET_PROTECTION_PERCENTAGE, getPercentCount(totalValue, coreReserveProtectionValues.get("protectionValue")));
-        distribution.put(TARGET_RESERVE_PERCENTAGE, getPercentCount(totalValue,  coreReserveProtectionValues.get("reserveValue")));
+        distribution.put(TARGET_RESERVE_PERCENTAGE, getPercentCount(totalValue, coreReserveProtectionValues.get("reserveValue")));
         return distribution;
     }
 
-    private List<String> concentrationProblems(Money totalValue , List<InstrumentObj> instrumentObjs ){
+    private List<String> concentrationProblems(Money totalValue, List<InstrumentObj> instrumentObjs) {
         List<String> concentrationProblems = new ArrayList<>();
-        for (InstrumentObj inst : instrumentObjs) {
-            if ("share".equals(inst.getType()) && !getCoreStockTicket().equals(inst.getTicker())) {
-                BigDecimal positionValue = inst.getQuantity().multiply(inst.getCurrentPrice().getValue());
-                BigDecimal percentage = getPercentCount(totalValue, positionValue);
-                if (percentage.compareTo(SATELLITE_CONCENTRATION_LIMIT.value) > 0) {
-                    String problem = String.format(
-                            "‼️ <b>Риск концентрации:</b> доля '%s' (<b>%s%%</b>) превышает лимит в <b>%s%%</b>!",
-                            inst.getName(), percentage.setScale(2, RoundingMode.HALF_UP), SATELLITE_CONCENTRATION_LIMIT.value
-                    );
-                    concentrationProblems.add(problem);
-                }
+        for (InstrumentObj inst : instrumentObjs.stream()
+                .filter(filter -> filter.getType().equals("share")).collect(Collectors.toSet())) {
+            BigDecimal positionValue = inst.getQuantity().multiply(inst.getCurrentPrice().getValue());
+            BigDecimal percentage = getPercentCount(totalValue, positionValue);
+            if (percentage.compareTo(SATELLITE_CONCENTRATION_LIMIT.value) > 0) {
+                String problem = String.format(
+                        "‼️ <b>Риск концентрации:</b> доля '%s' (<b>%s%%</b>) превышает лимит в <b>%s%%</b>!",
+                        inst.getName(), percentage.setScale(2, RoundingMode.HALF_UP), SATELLITE_CONCENTRATION_LIMIT.value
+                );
+                concentrationProblems.add(problem);
             }
         }
         return concentrationProblems;
     }
 
-    private Map<String,BigDecimal> setBondsCoreReserveProtectionValues(List<InstrumentObj> instrumentObjs){
-        Map<String,BigDecimal> values = new HashMap<>();
-        values.put("coreStockValue",BigDecimal.ZERO);
-        values.put("reserveValue",BigDecimal.ZERO);
-        values.put("protectionValue",BigDecimal.ZERO);
-        values.put("satelliteStockValue",BigDecimal.ZERO);
-        values.put("bondValue",BigDecimal.ZERO);
+    private Map<String, BigDecimal> setBondsCoreReserveProtectionValues(List<InstrumentObj> instrumentObjs) {
+        Map<String, BigDecimal> values = new HashMap<>();
+        values.put("coreStockValue", BigDecimal.ZERO);
+        values.put("reserveValue", BigDecimal.ZERO);
+        values.put("protectionValue", BigDecimal.ZERO);
+        values.put("satelliteStockValue", BigDecimal.ZERO);
+        values.put("bondValue", BigDecimal.ZERO);
         for (InstrumentObj inst : instrumentObjs) {
             BigDecimal positionValue = inst.getQuantity().multiply(inst.getCurrentPrice().getValue());
             if (getCoreStockTicket().equals(inst.getTicker())) {
-                values.replace("coreStockValue",values.get("coreStockValue").add(positionValue));
+                values.replace("coreStockValue", values.get("coreStockValue").add(positionValue));
             } else if (getReserveTickets().contains(inst.getTicker())) {
-                values.replace("reserveValue",values.get("reserveValue").add(positionValue));
+                values.replace("reserveValue", values.get("reserveValue").add(positionValue));
             } else if (getProtectionTickets().contains(inst.getTicker())) {
-                values.replace("protectionValue",values.get("protectionValue").add(positionValue));
-            }  else if ("share".equals(inst.getType())) {
-                values.replace("satelliteStockValue",values.get("satelliteStockValue").add(positionValue));
-            }else if ("bond".equals(inst.getType())) {
-                values.replace("bondValue",values.get("bondValue").add(positionValue));
+                values.replace("protectionValue", values.get("protectionValue").add(positionValue));
+            } else if ("share".equals(inst.getType())) {
+                values.replace("satelliteStockValue", values.get("satelliteStockValue").add(positionValue));
+            } else if ("bond".equals(inst.getType())) {
+                values.replace("bondValue", values.get("bondValue").add(positionValue));
             }
         }
         return values;
     }
 
-
-    /**
-     * Вспомогательный метод, который ИСПРАВЛЯЕТ ошибку сравнения.
-     * Он проверяет, выходит ли отклонение за рамки допуска, а не точное равенство.
-     */
     private void checkAndAddDeviation(Map<BalanceModuleConf, BigDecimal> deviations,
                                       BalanceModuleConf target,
                                       BigDecimal currentValue,
